17eae72186f948d8f75f01ed2cce0cbb2c8d6ef1
jujupew <81436543+jujupew@users.noreply.github.com>
2021-09-29 23:24:03 -0400

    Delete gkb4db-leetcode.py

--- gkb4db-leetcode.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def removeElement(self, nums, val):
-        while True: 
-            try:   # to avoid Value Error
-                cval = nums.index(val)
-            except: 
-                break
-            nums.pop(cval)
-        return len(nums)
